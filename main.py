b293d3f43808c261b349141d8b29002c123a6017
Angie <angel890817@hotmail.com>
2024-12-07 12:27:57 +1100

    Lesson done

---
+++ main.py
@@ -0,0 +1,7 @@
+# import module.my_module as spam
+# print(dir(spam))
+
+from color50 import rgb, constants
+
+my_color = rgb(128, 0, 128)
+print(constants.YELLOW + "Hello, World!" + constants.RESET)
